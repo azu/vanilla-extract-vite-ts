6620c1028529bdffdbde29b5b7434b0f99230e74
azu <azuciao@gmail.com>
2021-05-28 21:00:05 +0900

    init

---
+++ src/App.css.ts
@@ -0,0 +1,38 @@
+import { keyframes, style } from '@vanilla-extract/css'
+export const App = style({
+  textAlign: "center"
+}, "App");
+
+export const AooLogoSpin = keyframes({
+  from: {
+    transform: "0deg"
+  },
+  to: {
+    transform: "360deg"
+  }
+}, "AooLogoSpin");
+export const AppLogo = style({
+  height: "40vmin",
+  pointerEvents: "none",
+  "@media": {
+    "prefers-reduced-motion: no-preference": {
+      animation: `${AooLogoSpin} infinite 20s linear`
+    }
+  }
+}, "AppLogo");
+export const AppHeader = style({
+  backgroundColor: "#282c34",
+  minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  fontSize: "calc(10px + 2vmin)",
+  color: "white"
+}, "AppHeader");
+export const AppLink = style({
+  color: "#61dafb",
+}, "AppLink");
+export const Button = style({
+  fontSize: "calc(10px + 2vmin)"
+}, "Button");
---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import * as Style from './App.css';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={Style.App}>
+      <header className={Style.AppHeader}>
+        <img src={logo} className={Style.AppLogo} alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className={Style.AppLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className={Style.AppLink}
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
